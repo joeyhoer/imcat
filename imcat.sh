40bb90730aff51584ee74cbe084cafcedb64fed5
Joey Hoer <me@joeyhoer.com>
2023-04-11 10:52:10 -0400

    Adding resize option

--- imcat.sh
+++ imcat.sh
@@ -7,14 +7,14 @@ dir="-append"
 # Use Mac OS screen capture preferences by default
 name=$(defaults read com.apple.screencapture name 2>/dev/null)
 name=${name:-"Screen Shot"}
-location=$(defaults read com.apple.screencapture location 2>/dev/null)
+location=$(sh -c "echo $(defaults read com.apple.screencapture location 2>/dev/null)")
 location=${location:-"$HOME/Desktop"}
 type=$(defaults read com.apple.screencapture type 2>/dev/null)
 type=${type:-"png"}
 outfile="${location}/${name} $(date '+%Y-%m-%d at %H.%M.%S').${type}"
 
 # Get Options
-while getopts ":fVHg:b:o:p:" OPTION; do
+while getopts ":fVHRg:b:o:p:" OPTION; do
   case $OPTION in
     o) outfile=$OPTARG;;
     :) exit 1;;
@@ -46,15 +46,21 @@ fi
 if [[ ! "$OPT_V" == "" ]]; then
   dir="-append"
 
+  max_width=0
+  for img in "$@"; do
+    width=$(identify -format '%w' "$img")
+    if [ $width -gt $max_width ]; then
+      max_width=$width
+    fi
+  done
+
+  # Set resize
+  if [[ ! "$OPT_R" == "" ]]; then
+    resize="-resize ${max_width}x"
+  fi
+
   # Set gravity
   if [[ ! "$OPT_g" == "" ]]; then
-    max_width=0
-    for img in "$@"; do
-      width=$(identify -format '%w' "$img")
-      if [ $width -gt $max_width ]; then
-        max_width=$width
-      fi
-    done
     gravity="-gravity ${OPT_g} -extent ${max_width}x"
   fi
 
@@ -74,15 +80,21 @@ fi
 if [[ ! "$OPT_H" == "" ]]; then
   dir="+append"
 
+  max_height=0
+  for img in "$@"; do
+    height=$(identify -format '%h' "$img")
+    if [ $height -gt $max_height ]; then
+      max_height=$height
+    fi
+  done
+
+  # Set resize
+  if [[ ! "$OPT_R" == "" ]]; then
+    resize="-resize x${max_height}"
+  fi
+
   # Set gravity
   if [[ ! "$OPT_g" == "" ]]; then
-    max_height=0
-    for img in "$@"; do
-      height=$(identify -format '%h' "$img")
-      if [ $height -gt $max_height ]; then
-        max_height=$height
-      fi
-    done
     gravity="-gravity ${OPT_g} -extent x${max_height}"
   fi
 
@@ -99,4 +111,4 @@ if [[ ! "$OPT_H" == "" ]]; then
 fi
 
 # Concatenate images
-convert "$@" $background $splice $gravity $dir $chop $frame "$outfile"
+convert "$@" $resize $background $splice $gravity $dir $chop $frame "$outfile"
